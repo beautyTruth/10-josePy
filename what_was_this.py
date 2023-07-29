5ed5b058e76816eff3d40c7f9a6e7bda92baef8d
Phil Hasenkamp <89226935+beautyTruth@users.noreply.github.com>
2023-07-29 18:06:50 -0500

    complete section 10

---
+++ what_was_this.py
@@ -0,0 +1,6 @@
+def coffee(statement):
+    print(statement)
+
+so_much_to_say = "This section was about error handling. \nIt was more of a thing where we watched Jose, although \nwe could have coded along. I felt it was wiser \nto just watch him and pay attention."
+
+coffee(so_much_to_say)
